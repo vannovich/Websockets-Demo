361e120080b66a15ff1d7f2a27bd9c1f0935a92c
vannovich <awontuvannovich@gmail.com>
2026-04-18 09:37:57 -0400

    first Commit

---
+++ websockets-demo/index.js
@@ -0,0 +1,27 @@
+// import { request } from "node:http";
+import { WebSocketServer, WebSocket } from "ws";
+
+const ws = new WebSocketServer({ port: 8000 });
+
+// Connection Event
+ws.on("connection", (socket, request) => {
+  const ip = request.socket.remoteAddress;
+
+  socket.on("message", (rawData) => {
+    const message = rawData.toString();
+    console.log({ rawData });
+
+    ws.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN)
+        client.send(`Server Broadcast : ${message}`);
+    });
+  });
+
+  socket.on("error", (err) => {
+    console.log(`Error: ${err}: ${ip}`);
+  });
+
+  socket.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
